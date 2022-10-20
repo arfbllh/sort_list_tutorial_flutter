7de0f263ce1f896914afad01a9158bcbb0ed4a0f
riadath <akib152012@gmail.com>
2022-10-20 22:20:51 +0600

    adding buttons and dropdown menu

--- lib/main.dart
+++ lib/main.dart
@@ -26,7 +26,37 @@ class _MyAppState extends State<MyApp> {
     Pair(1, 'Demeter'),
     Pair(6, 'Athena'),
     Pair(5, 'Ares'),
+    Pair(17, 'Krabby'),
+    Pair(11, 'Ajwad'),
+    Pair(11, 'Patrick'),
+    Pair(69, 'Tomayto'),
+    Pair(43, 'Mr Bread'),
+    Pair(13, 'Geralt'),
+    Pair(17, 'Kittler'),
+    Pair(5, 'Greninja'),
+    Pair(1, 'Stealy'),
+    Pair(67, 'JohnCena'),
+    Pair(39, 'CabbageBabbage'),
+    Pair(77, 'Obama'),
+    Pair(12, 'Jigsaw'),
+    Pair(23, 'Yay'),
+    Pair(27, 'Sentinels'),
+    Pair(16, 'Thor'),
+    Pair(64, 'Spidermam'),
+    Pair(4, 'Shohag'),
+    Pair(25, 'YamCha'),
+    Pair(39, 'MMR'),
+    Pair(16, 'CleanMyTable'),
+    Pair(7, 'Pixel'),
   ];
+
+  //true : ascending, false: descending
+  bool order = true;
+  List<String> dropDown = [
+    'By Value',
+    'By Name',
+  ];
+  String curDropdownValue = 'By Value';
 
   @override
   Widget build(BuildContext context) {
@@ -37,7 +67,39 @@ class _MyAppState extends State<MyApp> {
             title: const Text('List Sorting Tutorial'),
           ),
           body: Center(
-            child: listviewBuilder(),
+            child: Column(
+              children: [
+                Row(mainAxisAlignment: MainAxisAlignment.center, children: [
+                  //Button For Changing sorting order
+                  TextButton.icon(
+                    icon: const Icon(Icons.compare_arrows_outlined),
+                    label: Text(order ? 'Ascending' : 'Descdending'),
+                    onPressed: () {
+                      setState(() {
+                        order = !order;
+                      });
+                    },
+                  ),
+                  //Drop Down menu for selecting which value to sort by
+                  DropdownButton<String>(
+                    value: curDropdownValue,
+                    items:
+                        dropDown.map<DropdownMenuItem<String>>((String value) {
+                      return DropdownMenuItem<String>(
+                        value: value,
+                        child: Text(value),
+                      );
+                    }).toList(),
+                    onChanged: (String? value) {
+                      setState(() {
+                        curDropdownValue = value!;
+                      });
+                    },
+                  ),
+                ]),
+                Expanded(child: listviewBuilder()),
+              ],
+            ),
           )),
     );
   }
@@ -45,7 +107,16 @@ class _MyAppState extends State<MyApp> {
   Widget listviewBuilder() => ListView.builder(
         itemCount: myList.length,
         itemBuilder: ((context, index) {
-          final currentItem = myList[index];
+          final sorted_list = myList;
+          if (curDropdownValue == 'By Value') {
+            if (order == true) {
+              
+            } else {}
+          } else {
+            if (order == true) {
+            } else {}
+          }
+          final currentItem = sorted_list[index];
           return ListTile(
             leading: CircleAvatar(
               radius: 20,
