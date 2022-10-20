544652dba6ec9cf243a17554904df7a3721e2ef3
riadath <akib152012@gmail.com>
2022-10-20 19:12:54 +0600

    adding listView builder1~

--- lib/main.dart
+++ lib/main.dart
@@ -18,25 +18,41 @@ class MyApp extends StatefulWidget {
 }
 
 class _MyAppState extends State<MyApp> {
-  Pair myPair = Pair(3, 'hello');
   List<Pair> myList = [
-    Pair(3, 'Hell'),
+    Pair(3, 'Posiden'),
     Pair(4, 'Hades'),
     Pair(7, 'Zeus'),
+    Pair(2, 'Hera'),
+    Pair(1, 'Demeter'),
+    Pair(6, 'Athena'),
+    Pair(5, 'Ares'),
   ];
 
   @override
   Widget build(BuildContext context) {
-  
     return MaterialApp(
       title: 'CSEDU',
       home: Scaffold(
           appBar: AppBar(
             title: const Text('List Sorting Tutorial'),
           ),
-          body: const Center(
-            child: Text('Hello#  '),
+          body: Center(
+            child: listviewBuilder(),
           )),
     );
   }
+
+  Widget listviewBuilder() => ListView.builder(
+        itemCount: myList.length,
+        itemBuilder: ((context, index) {
+          final currentItem = myList[index];
+          return ListTile(
+            leading: CircleAvatar(
+              radius: 20,
+              child: Text(currentItem.value.toString()),
+            ),
+            title: Text(currentItem.name),
+          );
+        }),
+      );
 }
